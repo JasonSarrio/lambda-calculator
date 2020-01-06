01d8abac4d56cb13d6391696b8246fd7f7207744
JasonSarrio <jsarrio16@gmail.com>
2020-01-06 13:19:42 -0600

    Reach MVP

--- src/components/ButtonComponents/NumberButtons/NumberButton.js
+++ src/components/ButtonComponents/NumberButtons/NumberButton.js
@@ -1,8 +1,8 @@
 import React from "react";
 
-const NumberButton = () => {
+const NumberButton = (props) => {
   return (
-    <>
+    <button>
       {/* Display a button element rendering the data being passed down from the parent container on props */}
       {props.text}
     </button>
--- src/components/ButtonComponents/SpecialButtons/Specials.js
+++ src/components/ButtonComponents/SpecialButtons/Specials.js
@@ -1,8 +1,11 @@
 import React from "react";
 
 //import any components needed
+import SpecialButton from "./SpecialButton.js";
 
 //Import your array data to from the provided data file
+
+import { specials } from "../../../data.js";
 
 const Specials = () => {
   // STEP 2 - add the imported data to state
